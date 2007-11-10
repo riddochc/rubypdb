1e4e25c91b45bdc86fb1331a9ef8a2395a33d383
Chris Riddoch <chris@syntacticsugar.org>
2007-11-09 18:11:30 -0700

    Implemented parsing of PDB headers with bit-struct
    This significantly improves the readability of the code, besides.
    A few tests seem to pass here.
    
    Signed-off-by: Chris Riddoch <chris@syntacticsugar.org>

--- lib/pdb.rb
+++ lib/pdb.rb
@@ -1,4 +1,5 @@
 require 'bit-struct'
+require 'enumerator'
 
 module PDB
 end
@@ -22,6 +23,11 @@ class PDB::AttributeFlags < BitStruct
   unsigned :resource,   1   #0x0001
 end
 
+class PDB::ResourceIndex < BitStruct
+  unsigned :next_index, 8 * 4
+  unsigned :number_of_records,    8 * 2
+end
+
 class PDB::Header < BitStruct
   text     :name,       8 * 32
   nest     :attributes, PDB::AttributeFlags
@@ -30,161 +36,158 @@ class PDB::Header < BitStruct
   unsigned :mtime,      8 * 4
   unsigned :baktime,    8 * 4
   unsigned :modnum,     8 * 4
-  unsigned :appinfo_off,   8 * 4
-  unsigned :sortinfo_off,  8 * 4
+  unsigned :appinfo_offset,   8 * 4
+  unsigned :sortinfo_offset,  8 * 4
   text     :type,       8 * 4
   text     :creator,    8 * 4
   unsigned :uniqueid,   8 * 4
+  nest     :resource_index, PDB::ResourceIndex
 end
 
+class PDB::Resource < BitStruct
+  text     :type,   8 * 4
+  unsigned :id,     8 * 2
+  unsigned :offset, 8 * 4
+end
+
+class PDB::RecordAttributes < BitStruct
+  unsigned :delete, 1  # 0x080
+  unsigned :dirty,  1  # 0x040
+  unsigned :busy,   1  # 0x020
+  unsigned :secret, 1  # 0x010
+  unsigned :attr4,  1  # 0x008
+  unsigned :attr3,  1  # 0x004
+  unsigned :attr2,  1  # 0x002
+  unsigned :attr1,  1  # 0x001
+end
+
+class PDB::Record < BitStruct
+  unsigned :offset, 8 * 4
+  nest     :attributes,  PDB::RecordAttributes
+  unsigned :id,     8 * 3
+end
+
+
+class PDB::StandardAppInfoBlock < BitStruct
+  unsigned  :renamed_categories,  8 * 2
+  16.times do |i|
+    text      "category_#{i}".to_sym,  8 * 16
+  end
+  16.times do |i|
+    unsigned  "category_#{i}_id".to_sym, 8 * 1
+  end
+  unsigned :last_unique_id, 8 * 1
+  rest     :rest
+end
+
+
 class PalmPDB
-  attr_reader :records, :app_info
+  attr_reader :records, :index
+  attr_accessor :header, :appinfo, :sortinfo
 
-  def initialize ()
+  def initialize()
     @index = []
+    @records = {}
+    @appinfo = nil
+    @sortinfo = nil
   end
 
-  def open_PDB_file(fname)
-    File.open(fname, "rb") do |f|
-      load_PDB(f)
-    end
-  end
-
-  def load_PDB(f)
+  def load(f)
     f.seek(0, IO::SEEK_END)
     @eof_pos = f.pos
     f.pos = 0  # return to beginning of file
 
-    header = f.read($header_length) # read the header
+    header_size = PDB::Header.round_byte_length  # 78
+    header_data = f.read(header_size)
+    @header = PDB::Header.new(header_data)   # Read the header.
 
-    fields = header.unpack("A32 n n N N N N N N a4 a4 N")
-    @name, @attrib, @version, @ctime, @mtime, @baktime,
-      @modnum, @appinfo_offset, @sort_offset, @type, @creator,
-      @uniqueIDseed = fields
- 
-    @ctime = Time.at(@ctime - $EPOCH_1904)
-    @mtime = Time.at(@mtime - $EPOCH_1904)
-    # @baktime = Time.at(@baktime)
+    # Next comes metadata for resources or records...
+    resource_size = PDB::Resource.round_byte_length
+    record_size = PDB::Record.round_byte_length
 
-    @attribute = Hash.new()
-    @@attribute_flags.each_pair { |flag, mask|
-      if ((@attrib & mask) != 0)
-        @attribute[flag] = true
+    @header.resource_index.number_of_records.times do |i|
+      if @header.attributes.resource == true
+        @index << PDB::Resource.new(f.read(resource_size))
       else
-        @attribute[flag] = false
+        @index << PDB::Record.new(f.read(record_size))
       end
-    }
-      
-    resource_index = f.read($record_index_header_length)
-
-    @next_index, @number_of_records = resource_index.unpack("N n")
-    if ((@attribute["resource"] == true) or (@attribute["ResDB"] == true))
-      load_resource_index(f)
-    else
-      load_record_index(f)
     end
 
-    first_record_offset = @index[0].offset
-    positions = []
-
-    if (@appinfo_offset != 0)
-      positions << PDB_block.new(:appinfo, @appinfo_offset)
+    # Now, a sanity check...
+    # Each entry in the index should have a unique offset.
+    # Which means, the number of unique offsets == number of things in the index
+    unless @index.collect {|i| i.offset }.uniq.length == @index.length
+      puts "Eeek.  Multiple records share an offset!"
     end
+    # And just to be sure they're in a sensible order...
+    @index = @index.sort_by {|i| i.offset }
     
-    if (@sort_offset != 0)
-      positions << PDB_block.new(:sortinfo, @sort_offset)
-    end
+    # The order of things following the headers:
+    #   1- Appinfo, Sortinfo, Records
+    #   2 - Appinfo, Records
+    #   3 - Sortinfo, Records
+    #   4 - Records
+    #
+    # Unfortunately, it's not necessarily clear what the length of any of these
+    # are ahead of time, so it needs to be calculated.
 
-    if (first_record_offset != 0)
-      positions << PDB_block.new(:first_record, first_record_offset)
-    end
-
-    positions << PDB_block.new(:eof, @eof_pos)
-
-    # Validating relative positions of blocks in the file
-    if positions != positions.sort
-      puts "Invalid PDB format"
-    end
-
-    positions.each_with_index do |block, index |
-      following = positions[index + 1]
-      unless following.nil?
-        block.length = following.position - block.position
-      end
-    end
-
-    positions.each do | block |
-      case block.name
-      when :appinfo
-        @appinfo_data = block.read_from_stream(f)
-
-        if (@appinfo_offset > 0)
-          @app_info = StdAppInfoBlock.new()
-          @app_info.parse(@appinfo_data)
-        end
-
-        parse_app_info(@app_info.other)
-      when :sortinfo
-         @sortinfo = block.read_from_stream(f)
-      when :first_record
-        f.pos = block.position
-        compute_record_sizes
-        load_items(f)
-      end
-    end
-
-    if ((@attribute["resource"] == true) or (@attribute["ResDB"] == true))
-      @resources = @index 
-    else
-      @records = @index
-    end
-    @index = nil
-
-    # puts self.to_yaml
-  end
-
-  def parse_app_info(data)
-    # Normally, do nothing.  Subclasses override
-  end
-
-  def load_resource_index(f)
-    @number_of_records.times do |i|
-      rie = @resourceclass.new(f)
-      @index << rie
-    end
-  end
-
-  def load_record_index(f)
-    lastoffset = 0
-    @number_of_records.times do |i|
-      rie = @recordclass.new(f)
-      if rie.offset == lastoffset
-        puts "Record #{i} has same offset as previous one: #{rie.offset}"
-      end
-      lastoffset = rie.offset
-      @index << rie
-    end
-  end
-
-  def load_items(f)
-    @index.each do | record |
-      if (f.pos != record.offset)
-        f.pos = record.offset
-      end
-      record.buffer = f.read(record.length)
-      record.load(self)
-    end
-  end
-
-  def compute_record_sizes()
-    @index.each_with_index do | entry, i |
-      following = @index[i + 1]
-      if following.nil?
-        entry.length = @eof_pos - entry.offset
+    appinfo_length = 0
+    sortinfo_length = 0
+    if @header.appinfo_offset > 0
+      if @header.sortinfo_offset > 0
+        appinfo_length = @header.sortinfo_offset - @header.appinfo_offset  # 1
+        sortinfo_length = @index.first.offset - @header.sortinfo_offset
       else
-        entry.length = following.offset - entry.offset
+        appinfo_length = @index.first.offset - @header.appinfo_offset      # 2
       end
+    elsif @header.sortinfo_offset > 0
+      sortinfo_length = @index.first.offset - @header.sortinfo_offset      # 3
     end
+
+    if appinfo_length > 0
+      f.pos = @header.appinfo_offset
+      @appinfo_data = f.read(appinfo_length)
+      @appinfo = PDB::StandardAppInfoBlock.new(@appinfo_data)
+    end
+
+    if sortinfo_length > 0
+      f.pos = @header.sortinfo_offset
+      @sortinfo_data = f.read(sortinfo_length)
+      @sortinfo = nil  # This really is app-specific, isn't it?
+    end
+
+    i = 0
+    @index.each_cons(2) do |curr, nxt|
+      length = nxt.offset - curr.offset  # Find the length to the next record
+      f.pos = curr.offset
+      data = f.read(length)
+      @records[curr.id] = data
+      i = i + 1 
+    end
+    # ... And then the last one.
+    entry = @index.last
+    f.pos = entry.offset
+    data = f.read()  # Read to the end
+    @records[entry.id] = data
+  end
+
+  def each(&block)
+    @index.each {|i|
+      yield i, @records[i.id]
+    }
+  end
+
+
+  $EPOCH_1904 = 2082844800
+  def ctime()
+    Time.at(@header.ctime - $EPOCH_1904)
+  end
+
+  def mtime()
+    Time.at(@header.mtime - $EPOCH_1904)
+  end
+
+  def backup_time()
+    Time.at(@header.baktime - $EPOCH_1904)
   end
 end
-
--- test/tc_pdbheader.rb
+++ test/tc_pdbheader.rb
@@ -3,16 +3,31 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'pdb.rb')
 
 require 'test/unit'
+require 'yaml'
+require 'parsedate'
 
 $datadir = File.join(File.dirname(__FILE__), 'data')
 
 class PDBHeaderTest < Test::Unit::TestCase
   def test_read
-    
+    a = PalmPDB.new()
     f = File.open($datadir + "/fuelLogDB.pdb")
-    header_data = f.read(72)  # The magic number; the header's 72 bytes long.
-    
-    header = PDB::Header.new(header_data)
-    puts header.inspect
+    a.load(f)
+    puts a.header.inspect
+    #puts a.appinfo.inspect
+    #a.each {|i, r|
+      #puts i.to_yaml
+      #puts FuelLogRecord.new(r).to_yaml
+    #}
+    # Did the parse work?
+    assert a.index.length == a.header.resource_index.number_of_records
+    assert a.records.length == a.header.resource_index.number_of_records
+    assert a.header.resource_index.number_of_records = 21
+
+    assert a.ctime == Time.local(*ParseDate.parsedate("2005-06-14 03:02:09"))
+    assert a.mtime == Time.local(*ParseDate.parsedate("2005-11-02 18:34:27"))
+    assert a.backup_time == Time.local(*ParseDate.parsedate("2005-10-07 03:52:39"))
+    assert a.header.attributes.backup == 1
+    assert a.header.attributes.read_only == 0
   end
 end
