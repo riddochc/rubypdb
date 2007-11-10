513ca070b57a90e5e7615587f28290279794b2df
Chris Riddoch <chris@syntacticsugar.org>
2007-11-09 18:12:15 -0700

    Created a PalmDate proxy object, between raw data and Date objects.
    Signed-off-by: Chris Riddoch <chris@syntacticsugar.org>

--- lib/common.rb
+++ lib/common.rb
@@ -1,6 +1,7 @@
 require 'date'
 require 'stringio'
 require 'bit-struct'
+require 'delegate'
 
 def convert_from_palm_date(d)
   if (d != 0xffff)
@@ -17,4 +18,55 @@ def convert_to_palm_date(d)
        ((d.month & 0x000f) << 5) |
        (((d.year - 1904) & 0x007f) << 9)
   return pd
+end
+
+
+class PalmDate < DelegateClass(Date)
+  def initialize(*rest)
+    if rest.length == 1
+      load(rest.first)
+    else
+      super(Date.new(*rest))
+    end
+  end
+
+  def load(io)
+    if io.kind_of? Integer
+      from_palm(io)
+    else
+      if io.respond_to? :read
+        data = io.read(2)
+      elsif io.respond_to? :length
+        data = io
+      end
+      from_palm(data.unpack('n')[0])
+    end
+  end
+
+  def dump(io = nil)
+    out = [to_palm()].pack('n')
+    if io.respond_to? :write
+      io.write(out)
+    end
+    return out
+  end
+
+  def from_palm(num)
+    if (num != 0xffff)
+      day = num & 0x001f
+      month = (num >> 5) & 0x000f
+      year = (num >> 9) & 0x07f
+      year += 1904
+      self.__setobj__(Date.new(year, month, day))
+    else
+      nil
+    end
+  end
+
+  def to_palm()
+    pd = (self.day & 0x001f) |
+       ((self.month & 0x000f) << 5) |
+       (((self.year - 1904) & 0x007f) << 9)
+    return pd
+  end
 end
