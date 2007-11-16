afadcf6f8eb3fc86e72a1041fe7ba998560334bc
Chris Riddoch <chris@syntacticsugar.org>
2007-11-16 16:49:35 -0700

    Cleaned up date/time handling, added tests.
    Signed-off-by: Chris Riddoch <chris@syntacticsugar.org>

--- lib/common.rb
+++ lib/common.rb
@@ -3,61 +3,27 @@ require 'stringio'
 require 'bit-struct'
 require 'delegate'
 
-def convert_from_palm_date(d)
-  if (d != 0xffff)
-    day = d & 0x001f
-    month = (d >> 5) & 0x000f
-    year = (d >> 9) & 0x07f
-    year += 1904
-    Date.new(year, month, day)
-  end
-end
+class Time
+  @@epoch_1904 = 2082844800
 
-def convert_to_palm_date(d)
-  pd = (d.day & 0x001f) |
-       ((d.month & 0x000f) << 5) |
-       (((d.year - 1904) & 0x007f) << 9)
-  return pd
+  def self.from_palm(secs)
+    self.at(secs - @@epoch_1904)
+  end
+
+  def to_palm()
+    @@epoch_1904 + self.to_i
+  end 
 end
 
 
-class PalmDate < DelegateClass(Date)
-  def initialize(*rest)
-    if rest.length == 1
-      load(rest.first)
-    else
-      super(Date.new(*rest))
-    end
-  end
-
-  def load(io)
-    if io.kind_of? Integer
-      from_palm(io)
-    else
-      if io.respond_to? :read
-        data = io.read(2)
-      elsif io.respond_to? :length
-        data = io
-      end
-      from_palm(data.unpack('n')[0])
-    end
-  end
-
-  def dump(io = nil)
-    out = [to_palm()].pack('n')
-    if io.respond_to? :write
-      io.write(out)
-    end
-    return out
-  end
-
-  def from_palm(num)
+class Date
+  def self.from_palm(num)
     if (num != 0xffff)
       day = num & 0x001f
       month = (num >> 5) & 0x000f
       year = (num >> 9) & 0x07f
       year += 1904
-      self.__setobj__(Date.new(year, month, day))
+      Date.new(year, month, day)
     else
       nil
     end
@@ -70,3 +36,4 @@ class PalmDate < DelegateClass(Date)
     return pd
   end
 end
+
---
+++ test/tc_datetime.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require File.join(File.dirname(__FILE__), '..', 'lib', 'common.rb')
+
+require 'test/unit'
+require 'yaml'
+require 'parsedate'
+
+class TimeTest < Test::Unit::TestCase
+  def test_time_class
+    palm_time_number = 3201584529
+
+    t = Time.local(*ParseDate.parsedate("2005-06-14 03:02:09 MDT"))
+    pt = Time.from_palm(palm_time_number)
+
+    assert (t == pt)
+    assert (pt.to_palm == palm_time_number)
+    assert (t.to_palm == pt.to_palm)
+  end
+
+  def test_date_class
+    a = Date.new(2005, 6, 14)
+    b = Date.from_palm(a.to_palm)
+    assert a == b
+  end
+end
