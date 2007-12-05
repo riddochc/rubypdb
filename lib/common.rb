8b144cf6eee227e88263db2a1f4688a24b83c5d6
Chris Riddoch <chris@syntacticsugar.org>
2007-12-05 15:43:12 -0700

    Put String#hexdump and String#to_hex in lib/common.rb
    Signed-off-by: Chris Riddoch <chris@syntacticsugar.org>

--- lib/common.rb
+++ lib/common.rb
@@ -37,3 +37,39 @@ class Date
   end
 end
 
+class String
+  def hexdump(params)
+    offset = params[:offset] || 0
+    width = params[:width] || 16
+    result = []
+    while raw = self.slice(offset, width) and raw.length > 0
+      # address field
+      line = sprintf("%08x  ", offset)
+
+      # data field
+      raw.each_byte {|c|
+        line << sprintf("%02x ", c)
+      }
+
+      line << (" " * (((width - raw.length) * 3) + 4))
+      # text field
+      line <<  raw.tr("\000-\037\177-\377", ".")
+
+      result << line
+      offset += width
+    end
+    result.join("\n")
+  end
+  
+  def to_hex(prefix = true)
+    if prefix == true
+      hex = "0x"
+    else
+      hex = ""
+    end
+    self.each_byte do |b|
+      hex << ("%x" % b)
+    end
+    hex
+  end
+end
