372cb29a3805c9c611311c4fbaa9a6fb1bce0b6e
Chris Riddoch <chris@syntacticsugar.org>
2007-12-06 16:10:18 -0700

    FuelLog as first user of new API, and created record manipulation tests.
    Signed-off-by: Chris Riddoch <chris@syntacticsugar.org>

--- lib/pdb-formats/fuellog.rb
+++ lib/pdb-formats/fuellog.rb
@@ -1,10 +1,59 @@
 require 'bit-struct'
 
-class FuelLogRecord < BitStruct
+require File.join(File.dirname(__FILE__), '..', 'pdb')
+require File.join(File.dirname(__FILE__), '..', 'common')
+
+
+class PDB::FuelLog < PalmPDB
+end
+
+class PDB::FuelLog::AppInfo < PDB::AppInfo
+  def initialize(*rest)
+    super(true, *rest)  # Uses standard categories.
+  end
+end
+
+class PDB::FuelLog::AppInfo::Struct < BitStruct
+  unsigned :fuel_volume, 8   #  0 = miles, 1 = kilometers
+  unsigned :distance_units, 8  #  0 = litres, 1 = gallons, 2 = US gallons
+  unsigned :efficiency_units, 8 #  0 = Miles/Gal, 1 = L/100km, 2 = km/L, 3 = km/gal
+  unsigned :category, 2*8
+  unsigned :fuel_dec, 2*8
+  unsigned :price_dec, 2*8
+  unsigned :odom_dec, 2*8
+end
+
+class PDB::FuelLog::Record < PDB::Data
+  def gallons
+    @struct.gallons.to_f / (10 ** @pdb.appinfo.struct.fuel_dec)
+  end
+
+  def price
+    @struct.total_price.to_f / (10 ** @pdb.appinfo.struct.price_dec)
+  end
+
+  def date
+    Date.from_palm(@struct.date)
+  end
+
+  def odometer
+    @struct.odometer
+  end
+
+  def fulltank
+    @struct.fulltank
+  end
+
+  def notes
+    @struct.notes
+  end
+end
+
+class PDB::FuelLog::Record::Struct < BitStruct
   unsigned :odometer, 4*8
   unsigned :gallons, 4*8
   unsigned :date, 2*8
-  unsigned :skip, 2*8
-  unsigned :total_price, 2*8
-  unsigned :flags, 2*8
+  unsigned :total_price, 4*8
+  unsigned :fulltank, 2*8
+  rest :notes
 end
---
+++ test/tc_recordmanip.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require File.join(File.dirname(__FILE__), '..', 'lib', 'pdb-formats', 'fuellog.rb')
+
+require 'test/unit'
+require 'yaml'
+require 'parsedate'
+require 'stringio'
+require 'tempfile'
+
+$datadir = File.join(File.dirname(__FILE__), 'data')
+
+class PDBHeaderTest < Test::Unit::TestCase
+  def test_load
+    a = PDB::FuelLog.new()
+    f = File.open($datadir + "/fuelLogDB.pdb")
+    a.load(f)
+    puts a.appinfo.struct.to_yaml
+    puts
+    a.each {|r|
+      puts "Date: #{r.date}"
+      puts "Odometer: #{r.odometer}"
+      puts "Gallons: #{r.gallons}"
+      puts "Price: #{r.price}"
+      puts "Filled tank: #{r.fulltank}"
+      # puts "Notes: #{r.notes}"
+      puts
+    }
+  end
+end
