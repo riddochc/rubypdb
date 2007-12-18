9668a8684bb51d46814381ea94bb7f20be01c2b3
Chris Riddoch <chris@syntacticsugar.org>
2007-12-18 16:20:15 -0700

    Initial support for memo format
    Signed-off-by: Chris Riddoch <chris@syntacticsugar.org>

---
+++ lib/pdb-formats/memo.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+
+require File.join(File.dirname(__FILE__), '..', 'pdb')
+require File.join(File.dirname(__FILE__), '..', 'common')
+require 'iconv'
+
+class PDB::MemoDB < PalmPDB
+end
+
+class PDB::MemoDB::AppInfo < PDB::AppInfo
+  def initialize(*rest)
+    super(true, *rest)  # Uses standard categories.
+  end
+end
+
+class PDB::MemoDB::Record < PDB::Data
+  def title()
+    @struct.title
+  end
+
+  def content()
+    @struct.content
+  end
+
+  def to_yaml(opts = {})
+    YAML::quick_emit( self.object_id, opts ) do |out|
+      out.map( to_yaml_style ) do |map|
+        map.add('Title', title())
+        map.add('Content', content())
+        map.add('Metadata', metadata_struct())
+      end
+    end
+  end
+end
+
+class PDB::MemoDB::Record::Struct
+  attr_accessor :title, :content
+
+  def initialize(data)
+    load(data)
+  end
+
+  def load(data)
+    data.strip!
+    @data = Iconv.conv("UTF-8", "Windows-1252", @data)
+    if data =~ /([^\n]*)\n+(.+)/m
+      @title = $1
+      @content = $2
+    else
+      @title = @content = data
+    end
+  end
+
+  def dump()
+    @title + "\n" + @content
+  end
+end
