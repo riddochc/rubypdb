d94266be2164239302bb8e9ad979b248209b70f2
Chris Riddoch <chris@syntacticsugar.org>
2007-12-23 12:36:53 -0700

    Updated an old test.
    Signed-off-by: Chris Riddoch <chris@syntacticsugar.org>

--- test/tc_binarystructs.rb
+++ test/tc_binarystructs.rb
@@ -13,27 +13,27 @@ class BitStructTest < Test::Unit::TestCase
 
     actual = {:odometer => 112523, :gallons => 9675,
               :date => Date.new(2005, 5, 8).to_palm,
-              :total_price => 22243, :flags => 1}
+              :total_price => 22243, :fulltank => 1}
 
     unpacked = entry.unpack("NNnnnn")
     odometer = unpacked[0]
     gallons = unpacked[1]
     date = unpacked[2]
     total_price = unpacked[4]
-    flags = unpacked[5]
+    fulltank = unpacked[5]
 
     assert_equal odometer, actual[:odometer]
     assert_equal gallons, actual[:gallons]
     assert_equal date, actual[:date]
     assert_equal total_price, actual[:total_price]
-    assert_equal flags, actual[:flags]
+    assert_equal fulltank, actual[:fulltank]
 
-    fuel_entry = FuelLogRecord.new(entry)
+    fuel_entry = PDB::FuelLog::Record::Struct.new(entry)
 
     assert_equal fuel_entry.odometer, actual[:odometer]
     assert_equal fuel_entry.gallons, actual[:gallons]
     assert_equal fuel_entry.date, actual[:date]
     assert_equal fuel_entry.total_price, actual[:total_price]
-    assert_equal fuel_entry.flags, actual[:flags]
+    assert_equal fuel_entry.fulltank, actual[:fulltank]
   end
 end
